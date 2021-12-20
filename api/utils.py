159c5ad6645825f7d90d19cf63c2d438880191bf
Melissa Farrell <caretcake@outlook.com>
2021-12-20 02:15:22 -0500

    add docstring

--- api/utils.py
+++ api/utils.py
@@ -1,4 +1,14 @@
 from decimal import Decimal
 
 def use_coin(coin_value: Decimal, dollar_amount: Decimal) -> bool:
+    """
+    Returns True or False depending on if given coin value should be used with dollar amount.
+
+        Parameters:
+            coin_value (Decimal): A decimal representing the given coin's value
+            dollar_amount (Decimal): A decimal representing the dollar amount remaining
+
+        Returns:
+            True if coin_value <= dollar_amount, False otherwise
+    """
     return coin_value.compare(dollar_amount) < 1
