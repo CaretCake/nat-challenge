0ce0abbd71beabf2df24a3dd40a73d6eae958c71
Melissa Farrell <caretcake@outlook.com>
2021-12-20 10:20:39 -0500

    add + update docstrings

--- api/utils.py
+++ api/utils.py
@@ -2,7 +2,7 @@ from decimal import Decimal, getcontext
 
 def use_coin(coin_value: Decimal, dollar_amount: Decimal) -> bool:
     """
-    Returns True or False depending on if given coin value should be used with dollar amount.
+    Returns True or False depending on if given coin value "fits" given dollar amount.
 
         Parameters:
             coin_value (Decimal): A decimal representing the given coin's value
@@ -14,6 +14,16 @@ def use_coin(coin_value: Decimal, dollar_amount: Decimal) -> bool:
     return coin_value.compare(dollar_amount) < 1
 
 def get_num_of_coin(coin_value: Decimal, dollar_amount: Decimal) -> int:
+    """
+    Returns number of coins of given value that "fit" in given dollar amount.
+
+        Parameters:
+            coin_value (Decimal): A decimal representing the given coin's value
+            dollar_amount (Decimal): A decimal representing the dollar amount remaining
+
+        Returns:
+            num_of_coins (Decimal): integer value representing number of coins that "fit" given dollar_amount
+    """
     getcontext().prec = 100
     num_of_coins = dollar_amount // coin_value
     getcontext().prec = 2
